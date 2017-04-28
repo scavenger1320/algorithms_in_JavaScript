9626205090dd6df95f80c48f45200da0eb07a470
Chris Ulanowicz <src3collector@gmail.com>
2017-04-28 08:58:30 -0500

    add singly linked list is empty?

---
+++ singly_linked_lists/sllIsEmpty.js
@@ -0,0 +1,28 @@
+var SLL = require("../singly_linked_lists/sllAdd.js"); // we need this file to access our singly linked list class definition
+// and it's functions to create a random list and add nodes
+
+// function that returns true/false if a list is empty
+SLL.prototype.isEmpty = function(){
+	if(!this.head){ // just check if the head is null
+		return true; // if it is then the list is empty so return true
+	}
+	else{ // else head is not null which means theres a node there
+		return false; // and thus not empty so return false
+	}
+}
+
+// same function in ternary
+SLL.prototype.isEmptyTern = function(){
+	return this.head ? false:true;
+}
+
+// test cases
+var testSll = new SLL(); // create our new empty list
+console.log(testSll.isEmpty()); // => true
+console.log(testSll.isEmptyTern()); // => true
+testSll.randomList(6); // add 6 random nodes into the list
+console.log(testSll.isEmpty()); // => false
+console.log(testSll.isEmptyTern()); // => false
+testSll.addBack(22); // add in another node
+console.log(testSll.isEmpty()); // => false
+console.log(testSll.isEmptyTern()); // => false
